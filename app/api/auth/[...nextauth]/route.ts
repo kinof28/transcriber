e0ae8e8227d652f5b2a833047c6aeb7b8f00a562
Abdelwahab <ab28fb@gmail.com>
2023-09-24 17:05:15 +0100

    init: next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+import NextAuth, { AuthOptions } from "next-auth";
+
+export const authOptions: AuthOptions = {
+  providers: [],
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
