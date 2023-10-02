92a66662ecc9d2e182eedc2d05f7f2702d2cd5fc
Abdelwahab <ab28fb@gmail.com>
2023-10-02 19:30:26 +0100

    Contact form input validation on user side

--- app/_components/UI/ContactForm.tsx
+++ app/_components/UI/ContactForm.tsx
@@ -3,26 +3,27 @@ import { useReducer } from "react";
 
 import Input from "./Input";
 import Button from "./Button";
+import type { ContactForm } from "@/models/contact.model";
+import { ContactSchema } from "@/models/contact.model";
+import { ZodError } from "zod";
 
-interface stateType {
-  email: string;
-  name: string;
-  subject: string;
-  message: string;
-}
 const actions = {
   setEmail: "SET_EMAIL",
   setName: "SET_NAME",
   setSubject: "SET_SUBJECT",
   setMessage: "SET_MESSAGE",
+  reset: "RESET",
 };
-const initialState = {
+const initialState: ContactForm = {
   email: "",
   name: "",
   subject: "",
   message: "",
 };
-const reducer = (state: stateType, action: { type: string; payload: any }) => {
+const reducer = (
+  state: ContactForm,
+  action: { type: string; payload?: any }
+) => {
   switch (action.type) {
     case actions.setEmail:
       return { ...state, email: action.payload };
@@ -32,6 +33,8 @@ const reducer = (state: stateType, action: { type: string; payload: any }) => {
       return { ...state, subject: action.payload };
     case actions.setMessage:
       return { ...state, message: action.payload };
+    case actions.reset:
+      return initialState;
     default:
       throw new Error("there is no action with the name: " + action.type);
   }
@@ -39,13 +42,37 @@ const reducer = (state: stateType, action: { type: string; payload: any }) => {
 
 const ContactForm = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [errors, dispatchErrors] = useReducer(reducer, initialState);
   const onSubmit = async () => {
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify(state),
-    });
-    const val = await response.json();
-    console.log(val);
+    if (validate("")) {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify(state),
+      });
+      const val = await response.json();
+      console.log(val);
+    } else {
+      console.log("invalid form inputs");
+    }
+  };
+  const validate = (
+    input: "" | "email" | "name" | "subject" | "message"
+  ): boolean => {
+    const parseResult = ContactSchema.safeParse(state);
+    if (input === "") dispatchErrors({ type: actions.reset });
+    else dispatchErrors({ type: `set_${input}`.toUpperCase(), payload: "" });
+
+    if (!parseResult.success) {
+      const errors = parseResult.error.errors;
+      errors.forEach((error) => {
+        if (input !== "" && input !== `${error.path[0]}`) return;
+        dispatchErrors({
+          type: `set_${error.path[0]}`.toUpperCase(),
+          payload: error.message,
+        });
+      });
+    }
+    return parseResult.success;
   };
   return (
     <div className="flex flex-col gap-2 pb-4 ">
@@ -53,9 +80,12 @@ const ContactForm = () => {
         type="email"
         value={state.email}
         placeholder="Your Email *"
-        onchange={(value: string) =>
-          dispatch({ type: actions.setEmail, payload: value })
-        }
+        onchange={(value: string) => {
+          dispatch({ type: actions.setEmail, payload: value });
+        }}
+        error={errors.email}
+        onblur={() => validate("email")}
+        onfocus={() => dispatchErrors({ type: actions.setEmail, payload: "" })}
       />
       {/* name */}
       <Input
@@ -65,7 +95,11 @@ const ContactForm = () => {
         onchange={(value: string) =>
           dispatch({ type: actions.setName, payload: value })
         }
+        onblur={() => validate("name")}
+        onfocus={() => dispatchErrors({ type: actions.setName, payload: "" })}
+        error={errors.name}
       />
+
       {/* subject */}
       <Input
         type="text"
@@ -74,6 +108,11 @@ const ContactForm = () => {
         onchange={(value: string) =>
           dispatch({ type: actions.setSubject, payload: value })
         }
+        onblur={() => validate("subject")}
+        onfocus={() =>
+          dispatchErrors({ type: actions.setSubject, payload: "" })
+        }
+        error={errors.subject}
       />
       {/* message */}
       <Input
@@ -83,7 +122,13 @@ const ContactForm = () => {
         onchange={(value: string) =>
           dispatch({ type: actions.setMessage, payload: value })
         }
+        onblur={() => validate("message")}
+        onfocus={() =>
+          dispatchErrors({ type: actions.setMessage, payload: "" })
+        }
+        error={errors.message}
       />
+
       <Button
         value="Send"
         className="bg-white text-indigo-700 border-2 border-indigo-300 hover:shadow-md hover:shadow-indigo-300"
--- app/_components/UI/Input.tsx
+++ app/_components/UI/Input.tsx
@@ -1,35 +1,52 @@
-const className =
-  "text-indigo-700 font-bold p-2 rounded-lg outline-none resize-none border-2 focus:border-indigo-300 focus:shadow-md focus:shadow-indigo-300 ";
-
 const Input = ({
   type,
   value,
   placeholder,
+  error,
   onchange,
+  onblur,
+  onfocus,
 }: {
   type: string;
   value: string;
   placeholder: string;
+  error?: string;
   onchange: any;
+  onblur?: any;
+  onfocus?: any;
 }) => {
-  return type === "textarea" ? (
-    <textarea
-      className={className}
-      name="message"
-      value={value}
-      placeholder={placeholder}
-      cols={30}
-      rows={10}
-      onChange={(e) => onchange(e.target.value)}
-    />
-  ) : (
-    <input
-      type={type}
-      value={value}
-      placeholder={placeholder}
-      onChange={(e) => onchange(e.target.value)}
-      className={className}
-    />
+  const className = `text-indigo-700 font-bold p-2 rounded-lg outline-none w-full resize-none border-2 focus:border-indigo-300 focus:shadow-md focus:shadow-indigo-300 ${
+    error && "border-pink-300 shadow-pink-300 shadow-md"
+  }`;
+  return (
+    <div className="relative">
+      <span className="text-xs font-serif text-red-600 z-10 absolute left-2 top-0">
+        {error}
+      </span>
+      {type === "textarea" ? (
+        <textarea
+          className={className}
+          name="message"
+          value={value}
+          placeholder={placeholder}
+          cols={30}
+          rows={10}
+          onChange={(e) => onchange(e.target.value)}
+          onBlur={() => onblur()}
+          onFocus={() => onfocus()}
+        />
+      ) : (
+        <input
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          onChange={(e) => onchange(e.target.value)}
+          onBlur={() => onblur()}
+          onFocus={() => onfocus()}
+          className={className}
+        />
+      )}
+    </div>
   );
 };
 
