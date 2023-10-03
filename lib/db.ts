efd26b2abb1446d09d6eea22d40d7e033c1b2b50
Abdelwahab <ab28fb@gmail.com>
2023-10-03 03:05:31 +0100

    rename prisma client as db

--- lib/db.ts
+++ lib/db.ts
@@ -10,8 +10,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export default prisma;
+export default db;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
